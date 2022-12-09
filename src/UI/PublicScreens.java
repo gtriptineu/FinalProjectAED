84349fd3281a6996627aaf8cd3727e017a0b79fa
Nikethana-Nagasubramanian <nikethanan01@gmail.com>
2022-12-08 23:36:00 -0500

    Commit

--- src/UI/PublicScreens.java
+++ src/UI/PublicScreens.java
@@ -42,9 +42,12 @@ public class PublicScreens extends javax.swing.JPanel {
         initComponents();
         jPanel1 = new JPanel();
         DefaultTableModel model = (DefaultTableModel) medicinesTable.getModel();
-        
+       
+        System.out.println("In public screen constructor");
+        System.out.println("Getting store dictionary"+ allStores.getStoreDictionary());
         for(Store store: allStores.getStoreDictionary())
         {
+        System.out.println("Details"+ store.getStoreName() + store.getCommunity());
         String storeName = store.getStoreName();
         String comm = store.getCommunity();
         String viewStore = "View Store";
@@ -254,7 +257,20 @@ public class PublicScreens extends javax.swing.JPanel {
             
             for(Store s: allStores.getStoreDictionary()){
                 System.out.println("--Store--"+ s.getStoreId()+"--"+s.getStoreName()+"--"+s.getCommunity());
+                String storeName = s.getStoreName();
+                String comm = s.getCommunity();
+                String viewStore = "View Store";
+                addRows(splitPane,storeName,comm,viewStore);
             }
+//            for(Store store: allStores.getStoreDictionary())
+//                {
+//                System.out.println("Details"+ store.getStoreName() + store.getCommunity());
+//                String storeName = store.getStoreName();
+//                String comm = store.getCommunity();
+//                String viewStore = "View Store";
+//                addRows(splitPane,storeName,comm,viewStore);
+//                setVisible(true);
+//                }
             
         } catch(SQLException e){
             System.out.println(e.getMessage());
