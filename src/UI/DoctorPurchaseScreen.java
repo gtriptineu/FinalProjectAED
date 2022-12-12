023449f9f49d090b370b6cc3a95ea2962db5882c
SeonaRodrigues <rodrigues.se@northeastern.edu>
2022-12-11 22:52:06 -0500

    minor changes

--- src/UI/DoctorPurchaseScreen.java
+++ src/UI/DoctorPurchaseScreen.java
@@ -29,16 +29,14 @@ public class DoctorPurchaseScreen extends javax.swing.JPanel {
     float totalPrice;
     
         public DoctorPurchaseScreen(JSplitPane jSplitPane, Doctor doctor, Inventory inv, int quantity, String storeIdPurchchased) {
-        initComponents();    
+            
         this.jSplitPane = jSplitPane;
         this.doctor = doctor;
         this.inv = inv;
         this.quantity = quantity;
         this.storeIdPurchased = storeIdPurchchased;
         
-//        docNameDisplay.setText(this.doctor.getDocName());
-//        contactDisplay.setText(this.doctor.getDocContact());
-//        addressDisplay.setText(this.doctor.getDocAddress());
+         initComponents();
         totalPrice = 0;
         System.out.println("inventory Details in const"+ this.inv.getMedicineID()+ this.inv.getMedicineName());
         populateTable(this.inv);
